b03d4857da55156756e5d09e14c3ddf8fb4d2383
Eren Taşçı <erent4sci@gmail.com>
2022-08-14 17:35:11 +0300

    Files added

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// Create random numbers
 
 let randomNumber1 = (Math.floor(Math.random() * 6) + 1); 
 let randomNumber2 = (Math.floor(Math.random() * 6) + 1);
-
-
-// Select <img> tag and add src attr. 
-
 
  document.querySelector(".img1").setAttribute("src",`./images/dice${randomNumber1}.png`);
  document.querySelector(".img2").setAttribute("src",`./images/dice${randomNumber2}.png`);
